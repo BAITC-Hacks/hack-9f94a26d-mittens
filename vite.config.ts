0b3b0f6bb9b0a878d6c8ca024cd1a5527dea678f
cloudlybtw <cloudlypower@gmail.com>
2026-09-23 17:53:48 +0500

    Configure Cloudflare Workers deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), viteReact()],
+  plugins: [cloudflare({ viteEnvironment: { name: 'ssr' } }), tanstackStart(), viteReact()],
 })
 
 export default config
